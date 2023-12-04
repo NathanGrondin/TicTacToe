6656b8a09b52a71f8f30c770adc9d62c97cdbec5
NathanGrondin <2352080@carrefour.cegepvicto.ca>
2023-12-04 09:17:48 -0500

    AlmostFinal

---
+++ js/Script.js
@@ -0,0 +1,7 @@
+const photoClique = document.getElementById('PhotoPerso');
+
+photoClique.addEventListener("click", emailMoi);
+
+function emailMoi(){
+    window.location.href = "mailto:2352080@etudiant.cegepvicto.ca"
+}
--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ const winningCombos = [
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
-    [0, 3, 6], 
+    [0, 3, 6],
     [1, 4, 7],
     [2, 5, 8],
     [0, 4, 8],
@@ -33,17 +33,26 @@ function getWinner() {
     winningCombos.forEach(function(combo, index) {
         if (board[combo[0]] && board[combo[0]] === board[combo[1]] && board[combo[0]] === board[combo[2]]) winner = board[combo[0]];
         });
+        /*Pour modifier le score*/
+        if (winner === "X" || winner === "O"){
+            ScoreUpdate(winner);
+        }
+
         return winner ? winner : board.includes('') ? null : 'T';
-};
+}
 
 function handleTurn() {
     let idx = squares.findIndex(function(square) {
         return square === event.target;
     });
-    board[idx] = turn;
-    turn = turn === 'X' ? 'O' : 'X';
-    win = getWinner();
-    render();
+
+    /* Ajouter "if" Pour éviter le remplacement de X ou O déja présent && Arreter quand quelqu'un gagne*/   
+    if(board[idx] === "" && win != "X" && win != "O"){
+        board[idx] = turn;
+        turn = turn === 'X' ? 'O' : 'X';
+        win = getWinner();
+        render();
+    }
 };
 
 function init() {
@@ -52,6 +61,7 @@ function init() {
     '', '', '',
     '', '', ''
     ];
+    win = getWinner(); /* ajouter pour pouvoir recomencer a mettre des X et O après une partie*/
     render();
 };
 
@@ -60,7 +70,32 @@ function render() {
     //this moves the value of the board item into the squares[idx]
     squares[index].textContent = mark;
     });
-    messages.textContent = win === 'T' ? `That's a tie, queen!` : win ? `${win} wins the game!` : `It's ${turn}'s turn!`;
+    messages.textContent = win === 'T' ? `Égalité!` : win ? `${win} à gagner!` : `C'est le tour à ${turn}`;
     };
 
 init();
+
+/* Fonction Pour Compter le score */
+
+localStorage.setItem("ScoreX", 0) /* Initialise les variable Pour le score*/
+localStorage.setItem("ScoreO", 0) /*                ///                   */
+
+function ScoreUpdate(winner) {
+    if (winner === 'X') { /* Regarde si X à gagné */
+        let textScoreX = document.getElementById('Victoire-X'); /* Associe avec HTML */
+        let scoreX = localStorage.getItem("ScoreX"); /* Associe avec Storage Local */
+        let winJoueurX = parseInt(scoreX); /* Transforme la valeur du stockage local en int */
+        winJoueurX += 1; /* Ajoute un au score de X */
+        localStorage.setItem("ScoreX", winJoueurX); /* Met la variable dans le stockage local */
+        textScoreX.textContent = winJoueurX; /* Affiche le score sur le site Web */
+
+    } else if (winner === 'O') { /* Regarde si O à gagné */
+
+        let textScoreO = document.getElementById('Victoire-O'); /* Associe avec HTML */
+        let scoreO = localStorage.getItem("ScoreO"); /* Associe avec Storage Local */
+        let winJoueurO = parseInt(scoreO); /* Transforme la valeur du stockage local en int */
+        winJoueurO += 1; /* Ajoute un au score de X */
+        localStorage.setItem("ScoreO", winJoueurO); /* Met la variable dans le stockage local */
+        textScoreO.textContent = winJoueurO; /* Affiche le score sur le site Web */
+    }
+}
